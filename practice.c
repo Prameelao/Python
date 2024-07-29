4b3bce371055d7cb1a740419111fba4a45dd0bba
prameelao <oprameeloruganti@gmail.com>
2024-07-29 12:25:59 +0530

    Practice programs done

---
+++ practice.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+    int a,b,res;
+    printf("enter a number: ");
+    scanf("%d",&a);
+    printf("enter a number: ");
+    scanf("%d",&b);
+    res=a/b;
+    printf("result is : %d",res);
+
+}
