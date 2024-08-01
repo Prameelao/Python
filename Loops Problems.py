720c45c2a95d400aee1ff46a8998c4cd560a3720
Prameelao <oprameelaoruganti@gmail.com>
2024-08-01 20:39:56 +0530

    loops problems done

---
+++ Loops Problems.py
@@ -0,0 +1,87 @@
+###Print First 10 Natural numbers
+##for num in range(1,11):
+##    print(num)
+
+
+###Print all evn numbers within range
+##for num in range(1,10):
+##    if num%2==0:
+##        print(num)
+##        
+
+
+##sum=0
+##for num in range(11):
+##    sum=sum+num
+##print(sum)
+##    
+
+
+###To print sum of ODD numbers in range
+##n=10
+##sum=0
+##for val in range(n+1):
+##    if val%2!=0:
+##        sum=sum+val
+##print(sum)
+
+
+###Print Multiplication table
+##num=12
+##n=10
+##for mul in range(n+1):
+##    print(num,"x",mul,'=',num*mul)
+
+
+###To display numbers from list using for loop
+##a=[1,2,"abc",(3,4),{6,7}]
+##for list in a:
+##    print(list)
+
+###count total number of digits in a number
+##num="123445"
+##count=0
+##for i in num:
+##    count=count+1
+##print(count)
+
+
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
