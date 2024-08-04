5a18f026bba1254856a2da38a0acb6fe39144f9b
Prameelao <oprameelaoruganti@gmail.com>
2024-08-04 21:09:52 +0530

    loops problems done

--- Loops Problems.py
+++ Loops Problems.py
@@ -45,8 +45,117 @@
 ##    count=count+1
 ##print(count)
 
+##
+##for num in range(0,41,5):
+##    if num==15:
+##      break
+##    else:
+##      print("num")
+##
+##
 
-#
+
+##num=10
+##count=0
+##for i in range(2,num//2):
+##    if num%i==0:
+##        count=count+1
+##        print(count,'=',count,'+',1)
+##if count==0:
+##    print('prime')
+##else:
+##    print('not prime')
+        
+
+##num=7
+##for i in range(2,num-1):
+##    if num%i==0:
+##        print('not a prime')
+##        break
+##else:
+##    print('prime')
+
+
+
+##num=7
+##for i in range(2,int(num**(0.5))+1):
+##    if num%i==0:
+##        print('not a prime')
+##        break
+##else:
+##    print('prime')
+
+
+
+#TO CHECK GIVEN NUMBER IS AMSTRONG NUMBER OR NOT
+##num=153
+##copy=num
+##power=len(str(num))
+##sum=0
+##while num!=0:
+##    rem=num%10
+##    rem=rem**power
+##    sum=sum+rem
+##    num=num//10
+##if sum==copy:
+##    print("amstrong")
+##else:
+##    print("not amstrong")
+##
+
+
+###TO CHECK NUBER IS A DISARM NUMBER OR NOT
+##num=1123
+##copy=num
+##sum=0
+##power=len(str(num))
+##while num!=0:
+##    rem=num%10
+##    rem=rem**power
+##    sum=sum+rem
+##    num=num//10
+##    power=power-1
+##if copy==sum:
+##    print("disarm")
+##else:
+##    print("not disarm")
+##    
+##
+
+
+num=123
+power=10**len(str(num))
+sum=0
+while num!=0:
+    rem=num%10
+    res=rem*power
+    sum=sum-res
+    num=num//10
+    power=power//10
+print(res)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
